e7017c62b172c04f25ce4d7621b259277ef2eaaa
ThisTrick <way.trickster@gmail.com>
2020-09-07 21:37:00 +0300

    Реализован функционал загрузки и сохранения изображений

---
+++ COVID-19_TemperatureScan/MainForm.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COVID_19_TemperatureScan
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pbResult_DoubleClick(object sender, EventArgs e)
+        {
+            if (pbResult.Image == null)
+            {
+                return;
+            }
+            ImageSave(pbResult);
+        }
+
+        private void pbRgb_DoubleClick(object sender, EventArgs e)
+        {
+            ImageLoad(pbRgb);
+        }
+
+        private void pbTemp_DoubleClick(object sender, EventArgs e)
+        {
+            ImageLoad(pbTemp);
+        }
+
+        /// <summary>
+        /// Сохраняет изображение в PictureBox.
+        /// </summary>
+        /// <param name="pictureBox">PictureBox</param>
+        void ImageSave(PictureBox pictureBox)
+        {
+            if (pictureBox.Image != null) //если в pictureBox есть изображение
+            {
+                //создание диалогового окна "Сохранить как..", для сохранения изображения
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                //отображать ли предупреждение, если пользователь указывает имя уже существующего файла
+                savedialog.OverwritePrompt = true;
+                //отображать ли предупреждение, если пользователь указывает несуществующий путь
+                savedialog.CheckPathExists = true;
+                //список форматов файла, отображаемый в поле "Тип файла"
+                savedialog.Filter = "Image Files(*.PNG)|*.PNG|Image Files(*.JPG)|*.JPG|Image Files(*.BMP)|*.BMP|Image Files(*.GIF)|*.GIF|All files (*.*)|*.*";
+                //отображается ли кнопка "Справка" в диалоговом окне
+                savedialog.ShowHelp = false;
+                if (savedialog.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+                {
+                    try
+                    {
+                        pictureBox.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Загружает изображение в PictureBox и переменную.
+        /// </summary>
+        /// <param name="image">Переменная изображения</param>
+        /// <param name="pictureBox">PictureBox</param>
+        Bitmap ImageLoad(PictureBox pictureBox)
+        {
+            //создание диалогового окна "Открыть изображение", для сохранения изображения
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Открыть изображение";
+            //отображать ли предупреждение, если пользователь указывает несуществующее название файла
+            openDialog.CheckFileExists = true;
+            //отображать ли предупреждение, если пользователь указывает несуществующий путь
+            openDialog.CheckPathExists = true;
+            //отображается ли кнопка "Справка" в диалоговом окне
+            openDialog.ShowHelp = false;
+            //список форматов файла, отображаемый в поле "Тип файла"
+            openDialog.Filter = "Image Files(*.PNG)|*.PNG|Image Files(*.JPG)|*.JPG|Image Files(*.BMP)|*.BMP|Image Files(*.GIF)|*.GIF|All files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+            {
+                try
+                {
+                    //Получаем путь к файлу
+                    var path = openDialog.FileName;
+                    //Получаем изображение 
+                    var image = new Bitmap(path);
+                    //Загружаем изображение в PictureBox
+                    pictureBox.Image = image;
+                    return image;
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть изображение", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            return null;
+        }
+    }
+}
